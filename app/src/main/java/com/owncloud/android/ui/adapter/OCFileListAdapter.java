a528bcfb1c8e11da33f58fb9c18323f5314dcd51
alperozturk <alper_ozturk@proton.me>
2025-11-17 10:44:28 +0100

    fix: empty list state visibility
    Signed-off-by: alperozturk <alper_ozturk@proton.me>
    
    # Conflicts:
    #	app/src/main/java/com/owncloud/android/ui/fragment/ExtendedListFragment.kt

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
@@ -938,6 +938,7 @@ public class OCFileListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         }
 
         searchType = null;
+        ocFileListFragmentInterface.toggleEmptyListViewState(mFiles.isEmpty() || mFilesAll.isEmpty());
         activity.runOnUiThread(this::notifyDataSetChanged);
     }
 
